d6a98f529dcc2b2009024856abf89bb275e6369f
aludpasopati-byte <aludpasopati@gmail.com>
2026-04-28 19:58:08 +0700

    public 3

--- Tugas praktikum .cpp
+++ Tugas praktikum .cpp
@@ -8,3 +8,13 @@ private:
     double d1, d2; // diagonal
     double s1, s2; // sisi
 
+public:
+    void input() {
+        cout << "Input Layang-Layang\n";
+        cout << "Diagonal 1: "; cin >> d1;
+        cout << "Diagonal 2: "; cin >> d2;
+        cout << "Sisi 1: "; cin >> s1;
+        cout << "Sisi 2: "; cin >> s2;
+    }
+
+   
