7def739398effb6899d47cd4d4f0cf73285e104f
aludpasopati-byte <aludpasopati@gmail.com>
2026-04-28 19:49:38 +0700

    pointer dua

--- PointerDua .cpp
+++ PointerDua .cpp
@@ -11,5 +11,22 @@ public:
     }
 };
 
+class mahasiswa
+{
+public:
+    int nim;
+    void showNim()
+    {
+        cout << "No Induk = " << nim << endl;
+    }
+};
 
+int main()
+{
+    mahasiswa *mhs = new mahasiswa{1}; // pointer object mhs
+    mhs->nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
+}
 }
