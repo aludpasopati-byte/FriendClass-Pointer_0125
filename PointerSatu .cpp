57b390ce1f64cf9818a560dec47f2069c0cbce90
aludpasopati-byte <aludpasopati@gmail.com>
2026-04-28 19:43:51 +0700

    int main

--- PointerSatu .cpp
+++ PointerSatu .cpp
@@ -8,3 +8,15 @@ class mahasiswa{
     }
 };
 
+int main(){
+    mahasiswa mhs{1}; //object mhs
+    mhs.showNim(); //member access operator
+    mahasiswa &refMhs = mhs; 
+    refMhs.nim = 2; 
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs;
+    pMhs->nim = 3;
+    pMhs->showNim();
+    return 0;
+}
